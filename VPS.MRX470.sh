c34b8665d6bb674974c5912bb60f40f4a0a9c92a
diego <145928215+vpsvip7@users.noreply.github.com>
2024-03-13 01:22:05 -0300

    Actualizar VPS.MRX470.sh

--- VPS.MRX470.sh
+++ VPS.MRX470.sh
@@ -40,8 +40,8 @@ tput setaf 3 ; tput bold ; echo "       INSTALANDO ADMINISTRADOR DE USUARIOS"; t
     chmod +x /bin/adm
 sleep 3
 tput setaf 7 ; tput bold ; echo "        INSTALANDO  GENERADOR DE BACKUP"; tput sgr0
-    wget https://github.com/vpsvip7/SSHPLUS/blob/main/Modulos/userbackup -O /bin/userbackup 1> /dev/null 2> /dev/stdout
-    chmod +x /bin/userbackup
+    wget https://github.com/vpsvip7/SSHPLUS/blob/main/Modulos/userbackup -O /usr/bin/userbackup 1> /dev/null 2> /dev/stdout
+    chmod +x /usr/bin/userbackup
 sleep 3
 tput setaf 4 ; tput bold ; echo "         INSTALANDO ADMINISTRADOR DE VPS"; tput sgr0
     wget https://raw.githubusercontent.com/vpsvip7/lite/main/adm2 -O /bin/adm2 1> /dev/null 2> /dev/stdout
